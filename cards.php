60e890b4fb6d59a46d5ee9c8fae254bd1ed409eb
Laura Bassani <bassanilaurarita@gmail.com>
2020-10-20 22:43:47 +0200

    Add a class and styles to cards

--- cards.php
+++ cards.php
@@ -4,40 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movies</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
 <body>   
 <?php
 class Movie {
-    private $title;
-    private $director;
-    private $rating;
+    public String $title;
+    public String $director;
+    public String $image;
 
-    function __construct($title, $director) {
+    public function __construct(String $title, String $director, String $image) 
+    {
         $this->title = $title;
         $this->director = $director;
-        $this->rating = 0;
-
-      }
-
-  function getTitle() {
-    return $this->title;
-  }
-
-  function getMovieInformation() {
-    return $this->title . $this->director;
-  }
+        $this->image = $image;
+    }
 }
 
-$titanic = new Movie("Titanic","James Cameron");
 
-echo 
-'<div class="card" style="width: 18rem;">';
-    '<div class="card-body">';
-    $titanic->getMovieInformation();
-    '</div>';
-'</div>';
+$titanicImage = 'titanic.jpg';
+$titanic = new Movie("Titanic", "James Cameron", $titanicImage);
+// echo 
+echo <<<TAG
+                <div class="col-lg-3 col-md-6">
+                    <article class='card' >
+                    <main class='card-body'>
+                    <h5 class="card-header">$titanic->title</h5>
+                    <p class="card-title">$titanic->director</p>
+                   <img class='card-img-top'src='${titanicImage}'/>
+                    </main>
+                    </article>
+                </div>
+                
+TAG;
 
-?>
+// $titanic->title . " - " . $titanic->director ."<br>";
+// echo '<img src='.${titanicImage}.'/>'
+    ?>
 </body>
 </html>
