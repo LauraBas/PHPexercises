4371929e6e99a2f67e354f55a7a115d7bf2f6efa
Laura Bassani <bassanilaurarita@gmail.com>
2020-10-21 08:38:01 +0200

    Add another movie and styles

--- cards.php
+++ cards.php
@@ -6,40 +6,53 @@
     <title>Movies</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 </head>
-<body>   
-<?php
-class Movie {
-    public String $title;
-    public String $director;
-    public String $image;
-
-    public function __construct(String $title, String $director, String $image) 
-    {
-        $this->title = $title;
-        $this->director = $director;
-        $this->image = $image;
+<style>
+    .container{
+        display: flex;
     }
-}
+</style>
+<body>   
+    <section>
+    <div class="container">
+    <?php
+    class Movie {
+        public String $title;
+        public String $director;
+        public String $image;
+
+        public function __construct(String $title, String $director, String $image) 
+        {
+            $this->title = $title;
+            $this->director = $director;
+            $this->image = $image;
+        }
+    }
 
 
-$titanicImage = 'titanic.jpg';
-$titanic = new Movie("Titanic", "James Cameron", $titanicImage);
-// echo 
-echo <<<TAG
-                <div class="col-lg-3 col-md-6">
-                    <article class='card' >
+
+    $titanic = new Movie("Titanic", "James Cameron",'titanic.jpg' );
+    $inception = new Movie("Inception", "Christopher Nolan",'inseption.jpg');
+
+    $films = [$titanic,$inception];
+
+    foreach ($films as $film){
+        echo <<<TAG
+        <div class="container">
+        <div "card border-primary mb-3" style="max-width: 18rem;"">
+            <article class='card' >
                     <main class='card-body'>
-                    <h5 class="card-header">$titanic->title</h5>
-                    <p class="card-title">$titanic->director</p>
-                   <img class='card-img-top'src='${titanicImage}'/>
+                        <h5 class="card-header">$film->title</h5>
+                        <p class="card-title">$film->director</p>
+                        <img class='card-img-top'src='$film->image'/>
                     </main>
-                    </article>
-                </div>
-                
-TAG;
-
-// $titanic->title . " - " . $titanic->director ."<br>";
-// echo '<img src='.${titanicImage}.'/>'
-    ?>
+                </article>
+            </div>
+            </div>
+            
+        TAG;
+    }
+        ?>
+    </div>
+    </section>
 </body>
 </html>
